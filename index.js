e4ceaa48803796fe0b319abb720fc2596c5cf0ff
Angelina <angelinarwhite@gmail.com>
2022-04-26 09:59:51 -0400

    commit tree e740efce13923570aab31446a3e853f818bf28f4

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solution in this file!
+
+function distanceFromHqInBlocks(dist)
+{
+    let blocks = Math.abs(dist - 42);
+    return blocks;
+}
+
+function distanceFromHqInFeet(dist)
+{
+    let feet = (Math.abs(dist - 42)) * 264;
+    return feet;
+}
+
+function distanceTravelledInFeet(start, end)
+{
+    let feet = (Math.abs(end - start)) * 264;
+    return feet;
+}
+
+function calculatesFarePrice(start, destination)
+{
+    let feet = (Math.abs(destination - start)) * 264;
+    let chargedFeet = feet - 400;
+    if (feet <= 400)
+    {
+        return 0;
+    }
+    else if (feet > 400 && feet <= 2000)
+    {
+        let price = chargedFeet * .02;
+        return price;
+    }
+    else if (feet > 2000 && feet < 2500)
+    {
+        return 25;
+    }
+    else{
+        return 'cannot travel that far';
+    }
+}
